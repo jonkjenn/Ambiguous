650d2c722c737d688fd8a28ae6e31c94e4f82bdf
jonkjenn <jonkjenn@gmail.com>
2014-01-23 12:17:56 +0100

    Editor set up initial sql usage with very basic editor view

--- Ambiguous/gen/no/hiof/android/ambiguous/R.java
+++ Ambiguous/gen/no/hiof/android/ambiguous/R.java
@@ -24,11 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int button1=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int card1=0x7f080005;
+        public static final int card_description=0x7f080003;
+        public static final int card_id=0x7f080001;
+        public static final int card_image=0x7f080004;
+        public static final int card_name=0x7f080002;
+        public static final int cardlayout=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int card=0x7f030000;
         public static final int editor=0x7f030001;
     }
     public static final class menu {

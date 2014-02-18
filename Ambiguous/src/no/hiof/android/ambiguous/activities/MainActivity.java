df342992474dc84d61bf34d966b3038bd278e856
Adrian Edlund <adrianedlun@hotmail.com>
2014-02-18 10:09:38 +0100

    Moved Network button to action bar. Toast pops up when choosing client or server

--- Ambiguous/src/no/hiof/android/ambiguous/activities/MainActivity.java
+++ Ambiguous/src/no/hiof/android/ambiguous/activities/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -18,7 +20,8 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
         return true;
     }
     
@@ -34,9 +37,15 @@ public class MainActivity extends Activity {
     	startActivity(intent);
     }
     
-    public void goToNetwork(View view)
-    {
+    /* Called when the user clicks the network action from the action bar */
+    public void onActionNetworkClicked(MenuItem menuItem){
     	Intent intent = new Intent(this, NetworkActivity.class);
     	startActivity(intent);
     }
+    
+    /*public void goToNetwork(View view)
+    {
+    	Intent intent = new Intent(this, NetworkActivity.class);
+    	startActivity(intent);
+    }*/
 }
--- Ambiguous/src/no/hiof/android/ambiguous/activities/NetworkActivity.java
+++ Ambiguous/src/no/hiof/android/ambiguous/activities/NetworkActivity.java
@@ -11,15 +11,23 @@ import java.util.List;
 import no.hiof.android.ambiguous.R;
 import no.hiof.android.ambiguous.network.Client;
 import no.hiof.android.ambiguous.network.Server;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
 import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.Toast;
 
+@SuppressLint("NewApi")
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class NetworkActivity extends Activity{
 	
 	@Override
@@ -27,6 +35,17 @@ public class NetworkActivity extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_network);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+    	MenuInflater inflater = getMenuInflater();
+        super.onCreateOptionsMenu(menu);
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH){
+        	getActionBar().setIcon(android.R.drawable.presence_offline);
+        }
+        return true;
 	}
 
 	private static List<String[]> getInterfaces()
@@ -99,12 +118,23 @@ public class NetworkActivity extends Activity{
 
 	private void doStartServer(String address)
 	{
-		Server s = new Server(address);		
+		Server s = new Server(address);	
+		// Pretending to tell you that a connection has been made
+		Toast.makeText(this, "Starting Server", Toast.LENGTH_SHORT).show();
+		getActionBar().setIcon(android.R.drawable.presence_online);
 	}
 	
 	public void startClient(View view)
 	{
-		Client c = new Client();		
+		Client c = new Client();	
+		// Pretending to tell you that a connection has been made
+		Toast.makeText(this, "Starting Client", Toast.LENGTH_SHORT).show();
+		getActionBar().setIcon(android.R.drawable.presence_online);
 	}
 
+	/* Called when the user clicks the network action from the action bar */
+    public void onActionNetworkClicked(MenuItem menuItem){
+    	Intent intent = new Intent(this, NetworkActivity.class);
+    	startActivity(intent);
+    }
 }

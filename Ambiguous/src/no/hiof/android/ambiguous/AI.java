a5018631921a19df4c890e54c326fbc831c60758
jonkjenn <jonkjenn@gmail.com>
2014-03-14 14:01:26 +0100

    Commented and slight optimization of AI

--- Ambiguous/src/no/hiof/android/ambiguous/AI.java
+++ Ambiguous/src/no/hiof/android/ambiguous/AI.java
@@ -6,30 +6,71 @@ import no.hiof.android.ambiguous.model.Card;
 import no.hiof.android.ambiguous.model.Effect;
 import no.hiof.android.ambiguous.model.Player;
 
+/**
+ * 
+ * Generates suggestions for which cards the computer should use.
+ *
+ */
+
 public class AI {
 	private Player computer;
 	private Player player;
 	private Card[] cards;
+	
+	private int resource;
+	private int damage;
+	private int armor;
+	private int heal;
+	private int moredamage;
 
 	public AI(Player computer, Player player) {
 		this.player = player;
 		this.computer = computer;
 
-		this.cards = new Card[computer.GetCards().length];
-
-		cards = computer.GetCards();
+		this.cards = computer.GetCards();
+		
+		findUseableEffects();
+	}
+	
+	/**
+	 * Finds the most effective card of each type we are able to cast with current resource.
+	 */
+	private void findUseableEffects()
+	{
+        resource = hasEffect(Effect.EffectType.RESOURCE, Effect.Target.SELF);
+        damage =hasEffect(Effect.EffectType.DAMAGE,Effect.Target.OPPONENT); 
+        armor =hasEffect(Effect.EffectType.ARMOR,Effect.Target.SELF);
+		heal = hasEffect(Effect.EffectType.HEALTH,Effect.Target.SELF);
+        moredamage = hasDamageMoreThenPlayer();
 	}
 
+	/**
+	 * Decides if computer will prefer aggressive choices or defensive choices.
+	 * 
+	 * @return The suggested card id to use. Or -1 if could not find any usable card.
+	 */
 	public int Start()
 	{
-        int resource = hasEffect(Effect.EffectType.RESOURCE, Effect.Target.SELF);
-        int damage =hasEffect(Effect.EffectType.DAMAGE,Effect.Target.OPPONENT); 
-        int armor =hasEffect(Effect.EffectType.ARMOR,Effect.Target.SELF);
-		int heal = hasEffect(Effect.EffectType.HEALTH,Effect.Target.SELF);
-        int moredamage = hasDamageMoreThenPlayer();
-
 		if(computer.getHealth() + computer.getArmor() > 100)
 		{
+			return playAggressive();
+			
+		}
+		else if(computer.getHealth() + computer.getArmor() <= 100)
+		{
+			return playDefensive();
+		}
+		
+		return -1;
+	}
+	
+	/**
+	 * Find aggressive card choice.
+	 * 
+	 * @return Card id or -1 if none.
+	 */
+	private int playAggressive()
+	{
 			if(moredamage >= 0)
 			{
 				return moredamage;				
@@ -49,9 +90,15 @@ public class AI {
 			
 			heal = hasHealLessMaxHP()[0];
 			if(heal >= 0){return heal;}
-		}
-		else if(computer.getHealth() + computer.getArmor() <= 100)
-		{
+			return -1;
+	}
+	
+	/**
+	 * Find defensive card choice.
+	 * @return Card id or -1 if none.
+	 */
+	private int playDefensive()
+	{
 			int[] armor_a= hasArmorLessMaxArmor();
 			int[] heal_a= hasHealLessMaxHP();
 			if(armor_a[0] >= 0 && heal_a[0] >= 0)
@@ -76,28 +123,49 @@ public class AI {
 			if(resource >=0){return resource;}
 			
 			if(damage>=0){return damage;}
-			
-		}
-		
-		return -1;
+			return -1;
 	}
 	
+	/**
+	 * Tries to find a card that on average does more damage then opponents current health.
+	 * 
+	 * @return Card id
+	 */
 	private int hasDamageMoreThenPlayer()
 	{
 		return hasminEffect(player.getHealth() + player.getArmor(),Effect.EffectType.DAMAGE,Effect.Target.OPPONENT)[0];
 	}
 	
+	/**
+	 * Tries to find a heal card that on average heals for less then computer missing health.
+	 * For preventing healing above max health.
+	 * 
+	 * @return An int array where position 0 is card id and position 1 is average heal amount.
+	 */
 	private int[] hasHealLessMaxHP() {
 		return hasEffect(computer.maxHealth - computer.getHealth(),
 				Effect.EffectType.HEALTH, Effect.Target.SELF);
 	}
 
+	/**
+	 * Tries to find a armor card that on average will give total armor below max armor limit.
+	 * 
+	 * @return An int array where position 0 is card id and position 1 is average armor amount.
+	 */
 	private int[] hasArmorLessMaxArmor() {
 		return hasEffect(computer.maxArmor - computer.getArmor(),
 				Effect.EffectType.ARMOR, Effect.Target.SELF);
 	}
 
 
+	/**
+	 * Tries to find a card that on average does the minimum amount of the effect type we want.
+	 * 
+	 * @param min_amount The minimum effect amount we need from the effect.
+	 * @param type The effect type.
+	 * @param target The effect target.
+	 * @return An int array where position 0 is card id and position 1 is average effect amount.
+	 */
 	private int[] hasminEffect(int min_amount, Effect.EffectType type,
 			Effect.Target target) {
 		int outcard = -1;
@@ -117,6 +185,16 @@ public class AI {
 		return new int[]{outcard,outamount};
 	}
 	
+
+	/**
+	 * 
+	 * Tries to find a card that on average does the most effect amount and the user has enough resources to use.
+	 * 
+	 * @param min_amount The effect amount we want from the effect.
+	 * @param type The effect type.
+	 * @param target The effect target.
+	 * @return An int array where position 0 is card id and position 1 is average effect amount.
+	 */
 	private int[] hasEffect(int amount, Effect.EffectType type,
 			Effect.Target target) {
 		int outcard = -1;
@@ -138,6 +216,11 @@ public class AI {
 		return new int[]{outcard,outamount};
 	}
 
+	/**
+	 * Calculates the average effect amount.
+	 * @param effect
+	 * @return Average effect amount.
+	 */
 	private int calcAvg(Effect effect) {
 		if(effect.getMaxValue()>0)
 		{
@@ -150,6 +233,13 @@ public class AI {
 		}
 	}
 
+	/**
+	 * Tries to find a card of the specified type which we have enough resources to use.
+	 * 
+	 * @param type Effect type.
+	 * @param target Effect target.
+	 * @return Card id or -1 if none.
+	 */
 	private int hasEffect(Effect.EffectType type, Effect.Target target) {
 		int outcard = -1;
 		for (int i = 0; i < cards.length; i++) {
@@ -169,6 +259,13 @@ public class AI {
 		return outcard;
 	}
 
+	/**
+	 * Tries to find a card with the specified effect types which we have enough resources to use.
+	 * 
+	 * @param type Effect type.
+	 * @param target Effect target.
+	 * @return Card id or -1 if none.
+	 */
 	private int hasEffect(List<Effect> effects, Effect.EffectType type,
 			Effect.Target target) {
 		for (int i = 0; i < effects.size(); i++) {
--- Ambiguous/src/no/hiof/android/ambiguous/MinigameFragment.java
+++ Ambiguous/src/no/hiof/android/ambiguous/MinigameFragment.java
@@ -1,14 +1,15 @@
 package no.hiof.android.ambiguous;
 
+import android.R.bool;
 import android.app.Fragment;
 import android.content.Context;
-import android.graphics.Matrix;
+import android.content.res.Configuration;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.renderscript.Matrix3f;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,13 @@ import android.widget.RelativeLayout.LayoutParams;
 public class MinigameFragment extends Fragment implements SensorEventListener {
 
 	View player;
+	private SensorManager sensorManager;
+	private Sensor sensor;
+	private int position;
+	private int speed = 1;
+	private int width;
+	
+	private boolean stop = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -34,9 +42,6 @@ public class MinigameFragment extends Fragment implements SensorEventListener {
 		return layout;
 	}
 
-	private SensorManager sensorManager;
-	private Sensor sensor;
-
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
@@ -45,108 +50,76 @@ public class MinigameFragment extends Fragment implements SensorEventListener {
 		player = getActivity().findViewById(R.id.minigame_player);
 		RelativeLayout.LayoutParams params = new LayoutParams(
 				player.getLayoutParams());
-		params.leftMargin = v.getLayoutParams().width / 2 - player.getWidth();
+		this.width = v.getLayoutParams().width;
+		position = this.width / 2 - player.getWidth();
+		params.leftMargin = position;
 		player.setLayoutParams(params);
+		setupSensor();
 
+		loop();
+	}
+	
+	private void setupSensor()
+	{
 		sensorManager = (SensorManager) getActivity().getSystemService(
 				Context.SENSOR_SERVICE);
-		sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+		sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 		sensorManager.registerListener(this, sensor,
 				SensorManager.SENSOR_DELAY_GAME);
-
+	}
+	
+	private void loop()
+	{
+		if(stop){return;}
+		Handler h = new Handler();
+		h.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				updatePosition(50);
+				loop();
+			}
+		},50);
+	}
+	
+	private void updatePosition(int duration)
+	{
+		player = getActivity().findViewById(R.id.minigame_player);
+		RelativeLayout.LayoutParams params = new LayoutParams(
+				player.getLayoutParams());
+		position += speed * duration/25;
+		params.leftMargin = position;
+		if(params.leftMargin + player.getWidth() >= this.width){speed = -1;}
+		player.setLayoutParams(params);
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
+		stop = true;
+		sensorManager.unregisterListener(this);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		stop = false;
+		setupSensor();
+		loop();
 	}
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
-
 	}
-
-	// Create a constant to convert nanoseconds to seconds.
-	private static final float NS2S = 1.0f / 1000000000.0f;
-	private static final float EPSILON = 0.1f;
-	private final float[] deltaRotationVector = new float[4];
-	private float timestamp;
-	private float[] topDir = new float[] { 1.0f,0,0,0,1.0f,0,0,0,1.0f};
-	private float[] result = new float[9];
 
 	public void onSensorChanged(SensorEvent event) {
 		
-		Log.d("rot",Float.toString(event.values[0]) + " " + Float.toString(event.values[1]) + " " + Float.toString(event.values[2]));
-		if(true){return;}
-		// This timestep's delta rotation to be multiplied by the current
-		// rotation
-		// after computing it from the gyro sample data.
-		if (timestamp != 0) {
-			final float dT = (event.timestamp - timestamp) * NS2S;
-			// Axis of the rotation sample, not normalized yet.
-			float axisX = event.values[0];
-			float axisY = event.values[1];
-			float axisZ = event.values[2];
-
-			// Calculate the angular speed of the sample
-			float omegaMagnitude = (float) Math.sqrt(axisX * axisX + axisY
-					* axisY + axisZ * axisZ);
-
-			// Normalize the rotation vector if it's big enough to get the axis
-			// (that is, EPSILON should represent your maximum allowable margin
-			// of error)
-			if (omegaMagnitude > EPSILON) {
-				axisX /= omegaMagnitude;
-				axisY /= omegaMagnitude;
-				axisZ /= omegaMagnitude;
-			}
-
-			// Integrate around this axis with the angular speed by the timestep
-			// in order to get a delta rotation from this sample over the
-			// timestep
-			// We will convert this axis-angle representation of the delta
-			// rotation
-			// into a quaternion before turning it into the rotation matrix.
-			float thetaOverTwo = omegaMagnitude * dT / 2.0f;
-			float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
-			float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
-			deltaRotationVector[0] = sinThetaOverTwo * axisX;
-			deltaRotationVector[1] = sinThetaOverTwo * axisY;
-			deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-			deltaRotationVector[3] = cosThetaOverTwo;
+		getResources().getConfiguration();
+		//Log.d("rot",Float.toString(event.values[0]) + " " + Float.toString(event.values[1]) + " " + Float.toString(event.values[2]));
+		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+		{
+			speed = -1 * (int)event.values[0];
 		}
-		timestamp = event.timestamp;
-		float[] deltaRotationMatrix = new float[9];
-		SensorManager.getRotationMatrixFromVector(deltaRotationMatrix,
-				deltaRotationVector);
-		// User code should concatenate the delta rotation we computed with the
-		// current rotation
-		// in order to get the updated rotation.
-		// rotationCurrent = rotationCurrent * deltaRotationMatrix;
-		
-		topDir = matrixMultiplication(topDir,deltaRotationMatrix);
-		Log.d("gyro", Float.toString(deltaRotationMatrix[0]) + " " + Float.toString(deltaRotationMatrix[4]) + " " + Float.toString(deltaRotationMatrix[8]));
-		//Log.d("gyro", Float.toString(topDir[0]) + " " + Float.toString(topDir[4]) + " " + Float.toString(topDir[8]));
-
-	}
-
-	private float[] matrixMultiplication(float[] a, float[] b)
-	{
-		float[] result = new float[9];
-
-		result[0] = a[0] * b[0] + a[1] * b[3] + a[2] * b[6];
-		result[1] = a[0] * b[1] + a[1] * b[4] + a[2] * b[7];
-		result[2] = a[0] * b[2] + a[1] * b[5] + a[2] * b[8];
-
-		result[3] = a[3] * b[0] + a[4] * b[3] + a[5] * b[6];
-		result[4] = a[3] * b[1] + a[4] * b[4] + a[5] * b[7];
-		result[5] = a[3] * b[2] + a[4] * b[5] + a[5] * b[8];
-
-		result[6] = a[6] * b[0] + a[7] * b[3] + a[8] * b[6];
-		result[7] = a[6] * b[1] + a[7] * b[4] + a[8] * b[7];
-		result[8] = a[6] * b[2] + a[7] * b[5] + a[8] * b[8];
-
-		return result;
 	}
 }

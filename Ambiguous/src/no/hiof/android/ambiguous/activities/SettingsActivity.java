4c25a63ca971e00e8030b5184c378badd26d21f8
Adrian Edlund <adrianedlun@hotmail.com>
2014-04-21 19:49:38 +0200

    Implemented backward compatibility for Settings. Settings for newer versions retains all of it's functionality

--- Ambiguous/src/no/hiof/android/ambiguous/activities/SettingsActivity.java
+++ Ambiguous/src/no/hiof/android/ambiguous/activities/SettingsActivity.java
@@ -1,135 +1,110 @@
 package no.hiof.android.ambiguous.activities;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
 import no.hiof.android.ambiguous.R;
 import no.hiof.android.ambiguous.fragments.SettingsFragment;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity implements
 		OnSharedPreferenceChangeListener {
+	
 	public static final String KEY_PREF_USER = "pref_user";
 	public static final String KEY_PREF_BGColor = "pref_bgcolor";
 	protected SettingsFragment settingsFragment;
 	private OnSharedPreferenceChangeListener listener;
-
+	protected Method mLoadHeaders = null;
+    protected Method mHasHeaders = null;
+    protected boolean boolTHing = false;
+	
+	/**
+     * Checks to see if using new v11+ way of handling PrefsFragments.
+     * @return Returns false pre-v11, else checks to see if using headers.
+     */
+    public boolean isNewV11Prefs() {
+        if (mHasHeaders!=null && mLoadHeaders!=null) {
+            try {
+                return (Boolean)mHasHeaders.invoke(this);
+            } catch (IllegalArgumentException e) {
+            } catch (IllegalAccessException e) {
+            } catch (InvocationTargetException e) {
+            }
+        }
+        return false;
+    }
+        
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
 
 		
-//		listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-//			@Override
-//			public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-//				
-//				if (key.equals(KEY_PREF_USER)) {
-//					String username = sharedPreferences.getString(key, "");
-//
-//					// Set summary to be the user-description for the selected value
-//					EditTextPreference editTextPref = (EditTextPreference) settingsFragment
-//							.findPreference(key);
-//					editTextPref.setSummary(username);
-//					//getFragmentManager().beginTransaction()
-//					//.replace(android.R.id.content, new SettingsFragment()).commit();
-//					
-//
-//				} else if (key.equals(KEY_PREF_BGColor)) {
-//					// Preference backgroundColor = findPreference(key);
-//					// // Set summary to be the user-description for the selected value
-//					// backgroundColor.setSummary(sharedPreferences.getString(key, ""));
-//				}
-//
-//			}
-//			
-//		};
+		//onBuildHeaders() will be called during super.onCreate()
+        try {
+            mLoadHeaders = getClass().getMethod("loadHeadersFromResource", int.class, List.class );
+            mHasHeaders = getClass().getMethod("hasHeaders");
+        } catch (NoSuchMethodException e) {
+        }
+        super.onCreate(savedInstanceState);
+        // This test does not recognize that I am actually using headers (I think)
+        if (!isNewV11Prefs()) {
+        	if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+        		addPreferencesFromResource(R.xml.preferences);
+        	}
+        	
+        }
 		
-		
-		// Add a button to the header list.
-		/*
-		 * if (hasHeaders()) { Button button = new Button(this);
-		 * button.setText("Some action"); setListFooter(button); }
-		 */
-		//String ss = getPreferences(MODE_PRIVATE).getString(KEY_PREF_USER, "");
-		settingsFragment = new SettingsFragment();
-		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-		/*if (!ss.isEmpty()) {
-			EditTextPreference editTextPref = (EditTextPreference) settingsFragment
-					.findPreference(KEY_PREF_USER);
-			editTextPref.setSummary(ss);
-		}*/
-
-		// Display the fragment as the main content.
-		getFragmentManager().beginTransaction()
-				.replace(android.R.id.content, settingsFragment).commit();
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+    		String ss = getPreferences(MODE_PRIVATE).getString(KEY_PREF_USER, "");
+			settingsFragment = new SettingsFragment();
+			PreferenceManager.setDefaultValues(this, R.xml.pref_headers, false);
 	
-//		PreferenceScreen ps2 = (PreferenceScreen) findPreference(KEY_PREF_BGColor);
-//		if(ps2 != null)
-//	    ps2.setOnPreferenceClickListener(this);
+			// Display the fragment as the main content.
+			getFragmentManager().beginTransaction()
+					.replace(android.R.id.content, settingsFragment).commit();
+			
+
+		}
 	}
 
 	
-//	@Override
-//	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-//		
-//		if (key.equals(KEY_PREF_USER)) {
-//			String username = sharedPreferences.getString(key, "");
-//
-//			// Set summary to be the user-description for the selected value
-//			EditTextPreference editTextPref = (EditTextPreference) settingsFragment
-//					.findPreference(key);
-//			editTextPref.setSummary(username);
-//			//getFragmentManager().beginTransaction()
-//			//.replace(android.R.id.content, new SettingsFragment()).commit();
-//			
-//
-//		} else if (key.equals(KEY_PREF_BGColor)) {
-//			// Preference backgroundColor = findPreference(key);
-//			// // Set summary to be the user-description for the selected value
-//			// backgroundColor.setSummary(sharedPreferences.getString(key, ""));
-//		}
-//
-//	}
+	@Override
+    public void onBuildHeaders(List<Header> aTarget) {
+        try {
+            mLoadHeaders.invoke(this,new Object[]{R.xml.pref_headers,aTarget});
+        } catch (IllegalArgumentException e) {
+        } catch (IllegalAccessException e) {
+        } catch (InvocationTargetException e) {
+        }  
+    }
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		/*if(getPreferenceScreen() != null)
-		if(((BaseAdapter)getPreferenceScreen().getRootAdapter() != null)){
-			((BaseAdapter)getPreferenceScreen().getRootAdapter()).notifyDataSetChanged();
-		}*/
-		//getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
 		getPreferences(MODE_MULTI_PROCESS).registerOnSharedPreferenceChangeListener(
 				this);
-//		PreferenceScreen ps2 = (PreferenceScreen) findPreference(KEY_PREF_BGColor);
-//		if(ps2 != null)
-//	    ps2.setOnPreferenceClickListener(this);
+
 		
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		//getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
-		
+
 		getPreferences(MODE_MULTI_PROCESS)
 				.unregisterOnSharedPreferenceChangeListener(this);
-		
-//		PreferenceScreen ps2 = (PreferenceScreen) findPreference(KEY_PREF_BGColor);
-//		if(ps2 != null)
-//	    ps2.setOnPreferenceClickListener(this);
+
 	}
-
 	
-
-	// Never called in SettingsActivity, only in SettingsFragment. Need to work out compatibility for < HONEYCOMB
+	// Never called in SettingsActivity, only in SettingsFragment.
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences arg0, String arg1) {
 		System.out.println();
 	}
-
 
 }
